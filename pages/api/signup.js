48071cedaabb30cac54d348fe57cbf45336bce4b
Henry Dorfman <henrydorfman@gmail.com>
2020-04-05 02:23:27 -0700

    The sign up route now sends a verification code to the user. References #7

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,21 +1,29 @@
 const { User } = require('../../models');
+const twilio = require('../../lib/twilio');
 
 export default async (req, res) => {
   if (req.method === 'POST') {
-    // generates random 4 character long alphanumeric string
-    req.body.verification_code = 'soft';
-
-    // insert into DB here
-    User.create(req.body)
-      .then((response) => res.json(response))
-      .catch((err) =>
-        res.status(409).json({
-          httpCode: 409,
-          message: 'User with this phone already exists',
+    if (!req.body.phone || req.body.phone.charAt(0) !== '+') {
+      res.status(400).json({
+        httpCode: 400,
+        message: 'Must provide valid phone number including + and country code',
+      });
+    } else {
+      // insert into DB here
+      User.create(req.body)
+        .then((response) => {
+          return twilio
+            .requestVerification(req.body.phone) // send verification text (twilio api call)
+            .then(() => Promise.resolve(response));
         })
-      );
-
-    // send verification text (twilio api call)
+        .then((response) => res.json(response))
+        .catch((err) => {
+          res.status(409).json({
+            httpCode: 409,
+            message: 'User with this phone already exists',
+          });
+        });
+    }
   } else {
     res.status(404).end();
   }
